3c82bd7422516c23472355ca38b97162ba70c917
Cielo <buen_cielo@naver.com>
2018-03-07 04:08:49 +0900

    SNS user Header Component not refactor

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Switch } from 'react-router';
 import Main from '../src/components/pages/main/';
 import Calc from '../src/components/pages/calc';
 import Todo from '../src/components/pages/todo';
+import SNS from '../src/components/projects/socialMedia/components/pages/main';
 
 class App extends React.Component {
   render() {
@@ -13,6 +14,7 @@ class App extends React.Component {
         <Route exact={true} path={'/main'} component={Main}/>
         <Route exact={true} path={'/calc'} component={Calc}/>
         <Route exact={true} path={'/todo'} component={Todo}/>
+        <Route exact={true} path={'/sns'} component={SNS} />
       </Switch>
     );
   }
